399a60283197418ce3c87bb93d8a2a8615fdc464
Rogerio Costa <rogerincosta1@gmail.com>
2025-06-25 17:26:07 -0300

    Incluindo os 3 testes de validação dos valores da nf

--- src/test/java/emissaoNf/EmissaoNfTest.java
+++ src/test/java/emissaoNf/EmissaoNfTest.java
@@ -3,6 +3,7 @@ package emissaoNf;
 import Pages.LoginPage;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
@@ -10,6 +11,10 @@ public class EmissaoNfTest {
 
     private WebDriver driver;
     Dotenv dotenv = Dotenv.load();
+
+    String dbUsuario = dotenv.get("DB_USER");
+    String dbSenha = dotenv.get("DB_PASSWORD");
+    String dbCnpjCliente = dotenv.get("DB_CNPJ_CLIENT");
 
     @BeforeEach
     public void beforeEach() {
@@ -25,35 +30,86 @@ public class EmissaoNfTest {
     }
 
     @Test
-    @DisplayName("Testando se valor total da nf está correto!")
-    public void testeSeValorTotalDaNfEstaCorreto() {
-
-        String dbUsuario = dotenv.get("DB_USER");
-        String dbSenha = dotenv.get("DB_PASSWORD");
-        String dbCnpjCliente = dotenv.get("DB_CNPJ_CLIENT");
+    @DisplayName("Validar o valor total da Nota fiscal!")
+    public void testeDeValidacaoDoValorTotalDaNf() {
 
         new LoginPage(driver)
-                .preencherUsuario(dbUsuario)
-                .preencherSenha(dbSenha)
+                .preencherUsuario(this.dbUsuario)
+                .preencherSenha(this.dbSenha)
                 .clicarParaIrAHomePage()
                 .fecharModalAviso()
                 .clicarParaPaginaListaNfs()
                 .clicarParaPaginaDeEmissaoDeNf()
-                .preencherCnpjCliente(dbCnpjCliente)
+                .preencherCnpjCliente(this.dbCnpjCliente)
                 .preencherCodigoServico("11")
-                .preencherValorUnitarioNf("38621,00")
+                .preencherValorUnitarioNf("38125,00")
                 .preencherQuantidadeServico("1")
                 .preencherDataPagamento("22/09/2025")
                 .preencherDescricaoServicoEPedido("PAGAMENTO LOGÍSTICA - PLACA DB7727", "45789900")
                 .clicarResponsavelIss()
-                .preencherValorPis("334,88")
-                .preencherValorConfins("12456,77")
-                .preencherValorCsll("245,99")
-                .preencherValorIr("245,99")
+                .preencherValorPis("247,81")
+                .preencherValorConfins("1143,75")
+                .preencherValorCsll("381,25")
+                .preencherValorIr("381,25")
                 .clicarOutrosTributos();
 
+        Assertions.assertEquals("38.125,00", driver.findElement(By.id("span_NFEVALORTOTAL")).getText());
 
-        //Assertions.assertEquals("38.125,00", driver.findElement(By.id("span_NFEVALORTOTAL")).getText());
+    }
+
+    @Test
+    @DisplayName("Validar o valor líquido da Nota fiscal!")
+    public void testeDeValidacaoDoValorLiquidoDaNf() {
+
+        new LoginPage(driver)
+                .preencherUsuario(this.dbUsuario)
+                .preencherSenha(this.dbSenha)
+                .clicarParaIrAHomePage()
+                .fecharModalAviso()
+                .clicarParaPaginaListaNfs()
+                .clicarParaPaginaDeEmissaoDeNf()
+                .preencherCnpjCliente(this.dbCnpjCliente)
+                .preencherCodigoServico("11")
+                .preencherValorUnitarioNf("38125,00")
+                .preencherQuantidadeServico("1")
+                .preencherDataPagamento("22/09/2025")
+                .preencherDescricaoServicoEPedido("PAGAMENTO LOGÍSTICA - PLACA DB7727", "45789900")
+                .clicarResponsavelIss()
+                .preencherValorPis("247,81")
+                .preencherValorConfins("1143,75")
+                .preencherValorCsll("381,25")
+                .preencherValorIr("381,25")
+                .clicarOutrosTributos();
+
+        Assertions.assertEquals("35.970,94", driver.findElement(By.id("span__NFEVALORLIQUIDONOTA")).getText());
+
+    }
+
+    @Test
+    @DisplayName("Validar os valores dos impostos a pagar na Nota fiscal!")
+    public void testeDeValidacaoDosValoresDosImpostos() {
+
+        new LoginPage(this.driver)
+                .preencherUsuario(this.dbUsuario)
+                .preencherSenha(this.dbSenha)
+                .clicarParaIrAHomePage()
+                .fecharModalAviso()
+                .clicarParaPaginaListaNfs()
+                .clicarParaPaginaDeEmissaoDeNf()
+                .preencherCnpjCliente(this.dbCnpjCliente)
+                .preencherCodigoServico("11")
+                .preencherValorUnitarioNf("38125,00")
+                .preencherQuantidadeServico("1")
+                .preencherDataPagamento("22/09/2025")
+                .preencherDescricaoServicoEPedido("PAGAMENTO LOGÍSTICA - PLACA DB7727", "45789900")
+                .clicarResponsavelIss()
+                .preencherValorPis("247,81")
+                .preencherValorConfins("1143,75")
+                .preencherValorCsll("381,25")
+                .preencherValorIr("381,25")
+                .clicarOutrosTributos();
+
+        Assertions.assertEquals("2.154,06", driver.findElement(By.id("span__OUTROSIMPOSTOS")).getText());
 
     }
 
