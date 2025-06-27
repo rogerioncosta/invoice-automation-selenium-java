d9939ea274d0946f12da7a8168b6716f23bd3124
Rogerio Costa <rogerincosta1@gmail.com>
2025-06-26 23:37:57 -0300

    Melhoria de legíbilidade do código, colocando a massa de testes em método separado

--- src/test/java/emissaoNf/EmissaoNfTest.java
+++ src/test/java/emissaoNf/EmissaoNfTest.java
@@ -6,32 +6,38 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EmissaoNfTest {
 
     private WebDriver driver;
     Dotenv dotenv = Dotenv.load();
+    WebDriverWait wait;
 
     String dbUsuario = dotenv.get("DB_USER");
     String dbSenha = dotenv.get("DB_PASSWORD");
     String dbCnpjCliente = dotenv.get("DB_CNPJ_CLIENT");
 
-    @BeforeEach
-    public void beforeEach() {
+    @BeforeAll
+    public void before() {
         // Abrir o navegador
         this.driver = new EdgeDriver();
 
         // Maximizar o navegador
         this.driver.manage().window().maximize();
 
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         String dbUrl = dotenv.get("DB_URL");
         // Navegar até a página web
         this.driver.get(dbUrl);
     }
 
-    @Test
-    @DisplayName("Validar o valor total da Nota fiscal!")
-    public void testeDeValidacaoDoValorTotalDaNf() {
+    public void preenchimentoDosDadosDaNf() {
 
         new LoginPage(driver)
                 .preencherUsuario(this.dbUsuario)
@@ -52,7 +58,14 @@ public class EmissaoNfTest {
                 .preencherValorCsll("381,25")
                 .preencherValorIr("381,25")
                 .clicarOutrosTributos();
+    }
 
+    @Test
+    @DisplayName("Validar o valor total da Nota fiscal!")
+    public void testeDeValidacaoDoValorTotalDaNf() {
+
+        // preenchimentoDosDadosDaNf();
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#span_NFEVALORTOTAL")));
         Assertions.assertEquals("38.125,00", driver.findElement(By.id("span_NFEVALORTOTAL")).getText());
 
     }
@@ -60,27 +73,10 @@ public class EmissaoNfTest {
     @Test
     @DisplayName("Validar o valor líquido da Nota fiscal!")
     public void testeDeValidacaoDoValorLiquidoDaNf() {
+        // Chama a massa de testes para preencher os dados, pois esse teste está sendo executado primeiro
+        preenchimentoDosDadosDaNf();
 
-        new LoginPage(driver)
-                .preencherUsuario(this.dbUsuario)
-                .preencherSenha(this.dbSenha)
-                .clicarParaIrAHomePage()
-                .fecharModalAviso()
-                .clicarParaPaginaListaNfs()
-                .clicarParaPaginaDeEmissaoDeNf()
-                .preencherCnpjCliente(this.dbCnpjCliente)
-                .preencherCodigoServico("11")
-                .preencherValorUnitarioNf("38125,00")
-                .preencherQuantidadeServico("1")
-                .preencherDataPagamento("22/09/2025")
-                .preencherDescricaoServicoEPedido("PAGAMENTO LOGÍSTICA - PLACA DB7727", "45789900")
-                .clicarResponsavelIss()
-                .preencherValorPis("247,81")
-                .preencherValorConfins("1143,75")
-                .preencherValorCsll("381,25")
-                .preencherValorIr("381,25")
-                .clicarOutrosTributos();
-
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#span__NFEVALORLIQUIDONOTA")));
         Assertions.assertEquals("35.970,94", driver.findElement(By.id("span__NFEVALORLIQUIDONOTA")).getText());
 
     }
@@ -89,32 +85,15 @@ public class EmissaoNfTest {
     @DisplayName("Validar os valores dos impostos a pagar na Nota fiscal!")
     public void testeDeValidacaoDosValoresDosImpostos() {
 
-        new LoginPage(this.driver)
-                .preencherUsuario(this.dbUsuario)
-                .preencherSenha(this.dbSenha)
-                .clicarParaIrAHomePage()
-                .fecharModalAviso()
-                .clicarParaPaginaListaNfs()
-                .clicarParaPaginaDeEmissaoDeNf()
-                .preencherCnpjCliente(this.dbCnpjCliente)
-                .preencherCodigoServico("11")
-                .preencherValorUnitarioNf("38125,00")
-                .preencherQuantidadeServico("1")
-                .preencherDataPagamento("22/09/2025")
-                .preencherDescricaoServicoEPedido("PAGAMENTO LOGÍSTICA - PLACA DB7727", "45789900")
-                .clicarResponsavelIss()
-                .preencherValorPis("247,81")
-                .preencherValorConfins("1143,75")
-                .preencherValorCsll("381,25")
-                .preencherValorIr("381,25")
-                .clicarOutrosTributos();
-
+        // preenchimentoDosDadosDaNf();
+        this.wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#span__OUTROSIMPOSTOS")));
         Assertions.assertEquals("2.154,06", driver.findElement(By.id("span__OUTROSIMPOSTOS")).getText());
 
     }
-
+/*
     @AfterEach
     public void afterEach() {
         driver.quit();
     }
+ */
 }
